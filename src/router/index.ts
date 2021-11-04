c936d5ce2105b8de35ed50aebfca441335d4a937
Tolkachev Alexsey <atolkachev@sportmaster.ru>
2021-11-04 23:22:14 +0300

    init

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
+import TheGame from '@/views/TheGame.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'TheGame',
+    component: TheGame
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/TheGame.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-game flex flex-col">
+    <h2>The Game</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
